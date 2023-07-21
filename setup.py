acfd5d2e9257f75d03fa5c4cbdda9dcfc711cba0
MarkyHan <114674936+MarkyHan@users.noreply.github.com>
2023-07-21 22:24:10 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+  
+setup(
+    name='my_package',
+    version='0.1',
+    description='A sample Python package',
+    author='Markéta Hanušková',
+    packages=['my_package'],
+    install_requires=[
+        ,
+    ],
+)
